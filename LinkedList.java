bb8ad52e2d9c0c35643ea7e1670afe8410f6cd9a
Sabbuds <sabbuds@gmail.com>
2024-02-13 17:39:48 -0500

    Added methods
    Added append, removeLast, and prepend.
    
    Plus some tests in Main

--- LinkedList.java
+++ LinkedList.java
@@ -31,7 +31,7 @@ public class LinkedList {
     public void printList() {
         Node temp = head;
         while (temp != null) {
-            System.out.println(temp.value);
+            System.out.print(temp.value + " ");
             temp = temp.next;
         }
     }
@@ -46,6 +46,55 @@ public class LinkedList {
 
     public void getLength() {
         System.out.println("Length: " + length);
+    }
+
+    // This method adds a number to the end of the Linked List.
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+
+        if (length == 0) { // In case the list is empty we want both pointers pointing to the new value.
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode; // This refers to the value being pointed at by the tail pointer
+            tail = newNode; // This refers to the actual pointer.
+        }
+        length++;
+    }
+
+    public Node removeLast() {
+        Node temp = head;
+        Node pre = temp;
+
+        if (length == 0 || head == null)
+            return null;
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+
+        }
+        return temp;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+
+        if (length == 0) { // This'll make both head and tail point to the new (only) node.
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head; // Makes new node point to where head pointer is currently pointing
+            head = newNode; // Makes head pointer point to the newNode
+        }
+        length++;
     }
 
 }
--- Main.java
+++ Main.java
@@ -2,12 +2,22 @@
 public class Main {
 
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(4);
+        LinkedList myLinkedList = new LinkedList(2);
 
+        myLinkedList.append(3);
         myLinkedList.getHead();
         myLinkedList.getTail();
         myLinkedList.getLength();
 
         myLinkedList.printList();
+
+        System.out.println("");
+
+        myLinkedList.prepend(1);
+
+        System.out.println("Removed: " + myLinkedList.removeLast().value); // This code prints the removed node.
+        System.out.println("Removed: " + myLinkedList.removeLast().value);
+        System.out.println(myLinkedList.removeLast()); // Notice how there is no .value This is because it is null.
+
     }
 }
